0f67b6f3ba229d56a8512f8090ceed37594d3ee1
Edgar <eujc21@gmail.com>
2020-03-24 22:32:21 -0400

    List of parameters imported, can call and start retrieving ranges from the list of objects

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import csv
 import request
 import redis
 from datetime import datetime
+from parameters import parameters
+
+# Get file going to parse through.
+
+# Create a celery task with a redis broker to deploy alerts
 
 def main():
+    parameters()
     script, file_name = argv
     with open(file_name) as f:
         reader = csv.reader(f)
         next(reader) # skip header
         data = []
         for row in reader:
-            print(row)
             data.append(row)
 
         
--- parameters.py
+++ parameters.py
@@ -1,37 +1,42 @@
 import sys
 import json
 
-parameters = [
+parameter_list = [
         {
-            key: "air_temperature",
-            high: 83.0,
-            low: 65.0
+            "key": "air_temperature",
+            "high": 83.0,
+            "low": 65.0
         },
         {
-            key: "humidity",
-            high: 45.0,
-            low: 90
+            "key": "humidity",
+            "high": 45.0,
+            "low": 90
         },
         {
-            key: "co2",
-            high:800.0,
-            low: 1300.0
+            "key": "co2",
+            "high":800.0,
+            "low": 1300.0
         },
         {
-            key: "ec_range",
-            high: 1000.0,
-            low: 2500
+            "key": "ec_range",
+            "high": 1000.0,
+            "low": 2500
         },
         {
-            key: "ph_range",
-            high: 5.7,
-            low: 7.0
+            "key": "ph_range",
+            "high": 5.7,
+            "low": 7.0
         },
         {
-            key: "water_temp",
-            high: 63.0,
-            low: 83.0
+            "key": "water_temp",
+            "high": 63.0,
+            "low": 83.0
         },
 ]
 
-class
+def parameters():
+    for parameter in parameter_list:
+        print(parameter["key"] == "air_temperature")
+
+if __name__ == "__main__":
+    parameters()
